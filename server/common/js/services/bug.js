384d038fb7436bb9cfa166246df3efda7ba0a121
Sean Christopherson <sean.j.c@gmail.com>
2017-11-06 18:52:36 -0800

    common: throw after logging bug, need to stop execution

--- server/common/js/services/bug.js
+++ server/common/js/services/bug.js
@@ -5,11 +5,13 @@ host.factory('bug', function () {
         bug: function(message) {
             console.trace();
             console.log(message);
+            throw new Error("BUG: " + message);
         },
         on: function(condition, message) {
             if (condition) {
                 console.trace();
                 console.log(message);
+                throw new Error("BUG: " + message);
             }
         }
     };
