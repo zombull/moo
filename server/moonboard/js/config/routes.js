10156a556a1c0fcdcb3711d55cd18a3d31f411d0
Sean Christopherson <sean.j.c@gmail.com>
2017-11-13 08:01:46 -0800

    moonboard: rename peruse.html to browse.html

--- server/moonboard/js/config/routes.js
+++ server/moonboard/js/config/routes.js
@@ -11,23 +11,23 @@ moon.config(function($routeProvider, $locationProvider) {
         controller: 'AirTrafficController as ctrl'
     })
     .when('/p/:grade', {
-        templateUrl: 'static/html/peruse.html',
+        templateUrl: 'static/html/browse.html',
         controller: 'PeruseController as ctrl'
     })
     .when('/t/:grade', {
-        templateUrl: 'static/html/peruse.html',
+        templateUrl: 'static/html/browse.html',
         controller: 'PeruseController as ctrl'
     })
     .when('/j/:grade', {
-        templateUrl: 'static/html/peruse.html',
+        templateUrl: 'static/html/browse.html',
         controller: 'PeruseController as ctrl'
     })
     .when('/s/:setter', {
-        templateUrl: 'static/html/peruse.html',
+        templateUrl: 'static/html/browse.html',
         controller: 'SetterController as ctrl'
     })
     .when('/st/:setter', {
-        templateUrl: 'static/html/peruse.html',
+        templateUrl: 'static/html/browse.html',
         controller: 'SetterController as ctrl'
     })
     .when('/:problem', {
