ec8a18abf85d47f34eba92694d54d9a6ca67d86f
Sean Christopherson <sean.j.c@gmail.com>
2017-11-14 21:42:38 -0800

    moonboard: userdata: sync pending mods prior to resolving get
    Annoying rework to ensure the result of synchronizing pending data
    is immediately reflected in the database, e.g. so the user doesn't
    have to refresh the page.

--- server/moonboard/js/services/userdata.js
+++ server/moonboard/js/services/userdata.js
@@ -15,25 +15,6 @@ moon.factory('userdata', function ($q, bug, storage, drive) {
         };
     }
 
-    // User data is stored on the user's Google Drive.  It's different from
-    // the data provided by the server in that it is retrieved every time.
-    // The major ramification of this is that there is a much higher chance
-    // of not being able to connect, e.g. due to lack of network.  So, don't
-    // completely freak out, we'll use the existing data in storage and will
-    // forego updates to Google Drive (so we don't waste time).
-    drive.get(USER_DATA, initData).then(
-        function(file) {
-            storage.set('projects', file.data.projects || {});
-            storage.set('ticks', file.data.ticks || {});
-            __drive.resolve(file.id);
-        },
-        function(error) {
-            __drive.reject();
-            console.log(error);
-            alert("Unabled to retrieve user data from Google Drive");
-        }
-    );
-
     var ls = {
         get: function() {
             return JSON.parse(localStorage.getItem('pending'));
@@ -42,6 +23,41 @@ moon.factory('userdata', function ($q, bug, storage, drive) {
             localStorage.setItem('pending', JSON.stringify(val));
         }
     };
+
+    function syncPending(id, data) {
+        var i = $q.defer();
+
+        var p = ls.get();
+        ls.set({});
+        if (_.isEmpty(p)) {
+            i.resolve(data);
+        } else {
+            drive.patch(id, p).then(
+                function(patchedData) {
+                    i.resolve(patchedData);
+                },
+                function(error) {
+                    bug.warn(true, error);
+                    var existing = ls.get();
+                    if (existing) {
+                        _.each(p, function(patches, index) {
+                            _.each(patches, function(patch, key) {
+                                if (!existing.hasOwnProperty(index) || !existing[index].hasOwnProperty(key)) {
+                                    existing[index] = p[index] || {};
+                                    existing[index][key] = patch;
+                                }
+                            });
+                        });
+                    } else {
+                        existing = p;
+                    }
+                    ls.set(existing);
+                    i.reject(error);
+                }
+            );
+        }
+        return i.promise;
+    }
 
     function setPending(index, key, val) {
         var p = ls.get();
@@ -54,34 +70,37 @@ moon.factory('userdata', function ($q, bug, storage, drive) {
         add: setPending,
         rm: setPending,
         sync: function() {
-            __drive.promise.then(function(id) {
-                var p = ls.get();
-                ls.set({});
-                if (!_.isEmpty(p)) {
-                    drive.patch(id, p).then(
-                        function() { },
-                        function(error) {
-                            bug.warn(true, error);
-                            var existing = ls.get();
-                            if (existing) {
-                                _.each(p, function(patches, index) {
-                                    _.each(patches, function(patch, key) {
-                                        if (!existing[index].hasOwnProperty(key)) {
-                                            existing[index][key] = patch;
-                                        }
-                                    });
-                                });
-                            } else {
-                                existing = p;
-                            }
-                            ls.set(existing);
-                        }
-                    );
-                }
-            });
+            __drive.promise.then(syncPending, function() { });
         }
     };
-    pending.sync();
+
+    // User data is stored on the user's Google Drive.  It's different from
+    // the data provided by the server in that it is retrieved every time.
+    // The major ramification of this is that there is a much higher chance
+    // of not being able to connect, e.g. due to lack of network.  So, don't
+    // completely freak out, we'll use the existing data in storage and will
+    // forego updates to Google Drive (so we don't waste time).
+    drive.get(USER_DATA, initData).then(
+        function(file) {
+            syncPending(file.id, file.data).then(
+                function(data) {
+                    storage.set('projects', data.projects || {});
+                    storage.set('ticks', data.ticks || {});
+                    __drive.resolve(file.id);
+                },
+                function(error) {
+                    __drive.reject();
+                    console.log(error);
+                    alert("Unabled to sync user data to Google Drive");
+                }
+            );
+        },
+        function(error) {
+            __drive.reject();
+            console.log(error);
+            alert("Unabled to retrieve user data from Google Drive");
+        }
+    );
 
     return {
         get: function() {
