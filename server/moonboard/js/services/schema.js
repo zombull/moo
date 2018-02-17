888d723d12480554d47089c50309907cb8980691
Sean Christopherson <sean.j.c@gmail.com>
2018-02-17 12:17:38 -0800

    moonboard: sync MD5 sums 02/17/2018

--- server/moonboard/js/services/schema.js
+++ server/moonboard/js/services/schema.js
@@ -16,10 +16,10 @@ moon.factory('schema', function () {
     };
     var checksums = {
         images: 'd12cd143a2d69484feaa72d1942bb979',
-        'index.problems': 'ed37aecd0455f6e1540fe798761c314a',
-        'index.setters': '8009c058736347e1c10336df35d0f1a3',
-        problems: '2b689d6acdead9516313fd304b81c84c',
-        setters: '0807df0a7942d1ddf40b9ed92bfc79a4',
+        'index.problems': '2385e473fa2b22472bb4855dc3ea2a6e',
+        'index.setters': '4ecf0dce3bd8aa74eaa2fd4c98586f2b',
+        problems: '2ae41acd4764948a78329c4153aef0f4',
+        setters: '43cf0e6b6750eaf00ca768a5731e2c24',
     };
 
     return {
