c4fd217c40b4dd40cbc0474326e60046ad357c5d
Sean Christopherson <sean.j.c@gmail.com>
2022-03-12 15:38:05 -0800

    moonboard: Download new wood hold images (Moonboard Masters 2019)
    Use a dedicated prefix for the wood holds to match upstream.  Note,
    plastic hold 201 is shifted to the empty slot 41 so that the match is
    nice and aligned for the wood holds.

--- server/moonboard/img/dl.sh
+++ server/moonboard/img/dl.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-for i in {1..149}; do
-	if [[ $i -lt 41 || $i -gt 49 ]]; then
-		curl -O https://moonboard.com/wp-content/plugins/moonboard/inc/img/$i.png
+for i in {1..148}; do
+	img=$i.png
+	if [[ ! -f "$img" ]]; then
+		if [[ $i -lt 41 || $i -gt 49 ]]; then
+			curl -o $img https://moonboard.com/content/images/holds/h$i.png
+		fi
+	fi
+done
+for i in {1..80}; do
+	img=w$i.png
+	if [[ ! -f "$img" ]]; then
+		if [[ $i -lt 10 ]]; then
+			curl -o $img https://moonboard.com/content/images/holds/hw0$i.png
+		else
+			curl -o $img https://moonboard.com/content/images/holds/hw$i.png
+		fi
 	fi
 done
